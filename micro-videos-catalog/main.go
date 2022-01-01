a11af8736bbd623421392620be8619f8c8ba8b92
xStrato <metalofgear@hotmail.com>
2022-01-01 14:57:56 -0300

    some main.go cleanup

--- micro-videos-catalog/main.go
+++ micro-videos-catalog/main.go
@@ -1,29 +1,5 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/xStrato/full-cycle-2-golang/micro-videos-catalog/domain/entities"
-)
-
 func main() {
-	c := entities.NewGenre("Horror")
-	c.AddCategory(entities.NewCategory("Movies"))
-	c.AddCategory(entities.NewCategory("Film"))
-	c.AddCategory(nil)
-	a := c.GetCategories()
-
-	a = append(a, *entities.NewCategory("Series"))
-	fmt.Printf("%v\n", c.GetCategories())
-	a[0] = *entities.NewCategory("Films")
-
-	fmt.Printf("%v\n", c.GetCategories())
-	fmt.Printf("%v\n", a)
-
-	v := entities.NewVideo("", "", 0, true)
-	v.AddT(entities.NewCategory("Movies"))
-	fmt.Printf("%v\n", v)
-
-	var cm entities.CastMember
-	fmt.Printf("%v\n", cm)
+	println("running")
 }
