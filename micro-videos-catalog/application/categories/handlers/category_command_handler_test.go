9b198299457ae3b4a12ae7ed0590978c80c01404
xStrato <metalofgear@hotmail.com>
2022-01-03 01:17:33 -0300

    added category handler mocks, stubs and tests

--- micro-videos-catalog/application/categories/handlers/category_command_handler_test.go
+++ micro-videos-catalog/application/categories/handlers/category_command_handler_test.go
@@ -1,11 +1,13 @@
 package handlers_test
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/require"
 	"github.com/xStrato/full-cycle-2-golang/micro-videos-catalog/application/categories/commands"
+	"github.com/xStrato/full-cycle-2-golang/micro-videos-catalog/application/categories/handlers"
 	"github.com/xStrato/full-cycle-2-golang/micro-videos-catalog/domain/entities"
 	mock_interfaces "github.com/xStrato/full-cycle-2-golang/micro-videos-catalog/mocks"
 )
@@ -13,39 +15,61 @@ import (
 func TestCategoryCommandHandler(t *testing.T) {
 	ctrl := gomock.NewController(t)
 	defer ctrl.Finish()
-	repository := mock_interfaces.NewMockCategoryRepository(ctrl)
 
-	categories := []entities.Category{
+	repository := mock_interfaces.NewMockCategoryRepository(ctrl)
+	handler := handlers.NewCategoryCommandHandler(repository)
+
+	t.Run("Handle_ValidCreateCategoryCommandParams_ShouldCreateAndPersistCommand", func(t *testing.T) {
+		// Arrange
+		categories := generateNewCategorySlice()
+		var createdCategory *entities.Category
+
+		repository.EXPECT().Commit().Return(nil).MaxTimes(1)
+		repository.EXPECT().Add(gomock.Any()).MaxTimes(1).DoAndReturn(func(e *entities.Category) error {
+			createdCategory = e
+			categories = append(categories, *e)
+			return nil
+		})
+
+		cmd := commands.NewCreateCategoryCommand("Movie")
+		expectedMessage := fmt.Sprintf("%v was successfully executed", cmd.GetCommandType())
+		// Act
+		result, err := handler.Handle(cmd)
+		//Assert
+		require.Nil(t, err)
+		require.NotNil(t, createdCategory)
+		require.True(t, result.HasSuccess())
+		require.Equal(t, expectedMessage, result.GetMessage())
+		require.IsType(t, &entities.Category{}, result.GetData())
+		require.Len(t, categories, 6)
+	})
+
+	t.Run("Handle_InvalidCreateCategoryCommandParams_ShouldReturnErrorNotNil", func(t *testing.T) {
+		// Arrange
+		categories := generateNewCategorySlice()
+		repository.EXPECT().Add(gomock.Any()).MaxTimes(0)
+		repository.EXPECT().Commit().MaxTimes(0)
+
+		cmd := commands.NewCreateCategoryCommand("Mo")
+		expectedMsg := fmt.Sprintf("%v state is invalid", cmd.GetCommandType())
+		expectedDataMsg := "Name: Mo does not validate as length(3|20)"
+		// Act
+		result, err := handler.Handle(cmd)
+		//Assert
+		require.Nil(t, err)
+		require.False(t, result.HasSuccess())
+		require.Equal(t, expectedMsg, result.GetMessage())
+		require.Equal(t, expectedDataMsg, result.GetData())
+		require.Len(t, categories, 5)
+	})
+}
+
+func generateNewCategorySlice() []entities.Category {
+	return []entities.Category{
 		*entities.NewCategory("Movie"),
 		*entities.NewCategory("Vlog"),
 		*entities.NewCategory("Serie"),
 		*entities.NewCategory("Documentary"),
 		*entities.NewCategory("Animation"),
 	}
-	t.Run("Handle_ValidCreateCategoryCommandParams_ShouldCreateAndPersistCommand", func(t *testing.T) {
-		// Arrange
-		name := "Movie"
-		// Act
-		c1 := commands.NewCreateCategoryCommand(name)
-		repository.EXPECT().Add(gomock.Any()).Return(100).AnyTimes()
-		//Assert
-		require.NotNil(t, c1)
-		require.Len(t, categories, 6)
-		require.NotNil(t, c1.Command)
-		require.Nil(t, c1.IsValid())
-		require.Equal(t, c1.GetCommandType(), "CreateCategoryCommand")
-	})
-
-	t.Run("Handle_InvalidCreateCategoryCommandParams_ShouldReturnErrorNotNil", func(t *testing.T) {
-		// Arrange
-		name := "Mo"
-		// Act
-		c1 := commands.NewCreateCategoryCommand(name)
-		//Assert
-		require.NotNil(t, c1)
-		require.Len(t, categories, 5)
-		require.NotNil(t, c1.Command)
-		require.NotNil(t, c1.IsValid())
-		require.Equal(t, c1.GetCommandType(), "CreateCategoryCommand")
-	})
 }
